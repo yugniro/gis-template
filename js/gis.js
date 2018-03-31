cc80204112f6b137e12cf0ece843ac0e8a24a3a4
zenn <zenn1989@gmail.com>
2018-03-31 10:45:22 +0300

    Add zoom buttons; add multiline geojson features

--- js/gis.js
+++ js/gis.js
@@ -1,37 +1,77 @@
 var lay = {};
+var owsurl = 'https://map.azniirkh.ru/geoserver/azniirkh_zones/ows';
 
-// extend leaflet geoJSON method: draw 1px point radius with label
-var pointLayer = L.geoJSON(null, {
-    pointToLayer: function(feature,latlng){
-        var label = String(feature.properties.name); // attribute: name="text"
-        return new L.CircleMarker(latlng, {
-            radius: 1
-        }).bindTooltip(label, {permanent: true}).openTooltip();
-    }
-});
-
-
-function addGeojsonPoints(name, url, map) {
-    // add layer from local key-value storage
+function displayLayerType(type, name, map) {
+    var url;
+    // check if layer is already loaded
     if (name in lay) {
         map.addLayer(lay[name]);
-        return;
+        return true;
     }
 
-    // load layer over jsonp
+    // geojson -> points
+    if (type === 'point' || type === 'points') {
+        addGeojsonPoints(name, map);
+        return true;
+    }
+    // geojson -> multiline
+    if (type === 'multiline' || type === 'lines' || type === 'multilines') {
+        addGeojsonLines(name, map);
+        return true;
+    }
+
+
+    return false;
+}
+
+function addGeojsonLines(name, map) {
     $.ajax({
-        url: url,
+        url: owsurl + '?service=WFS&version=1.0.0&request=GetFeature&typeName=' + name + '&maxFeatures=1000&outputFormat=text/javascript',
         dataType: 'jsonp',
         jsonpCallback: 'parseResponse',
         success: function(data) {
-            pointLayer.addData(data);
-            map.addLayer(pointLayer);
-            lay[name] = pointLayer;
+            var layer = L.geoJSON(data, {
+                onEachFeature: function(feature, layer) {
+                    if (feature.properties && feature.properties.name) {
+                        layer.bindPopup('<strong>' + feature.properties.name + '</strong><br /><p>' + feature.properties.desc + '</p>');
+                    }
+                }
+            });
+            map.addLayer(layer);
+            lay[name] = layer;
+        }
+    })
+}
+
+/**
+ * Add leaflet layer from OWS geojson response - points
+ * @param name
+ * @param map
+ */
+function addGeojsonPoints(name, map) {
+    // load layer over jsonp
+    $.ajax({
+        url: owsurl + '?service=WFS&version=1.0.0&request=GetFeature&typeName=' + name + '&maxFeatures=1000&outputFormat=text/javascript',
+        dataType: 'jsonp',
+        jsonpCallback: 'parseResponse',
+        success: function(data) {
+            var layer = L.geoJSON(data, {
+                pointToLayer: function(feature, latlng){
+                    var lbl = String(feature.properties.name);
+                    return new L.CircleMarker(latlng, {
+                        radius: 1
+                    }).bindTooltip(lbl, {permanent: true}).openTooltip();
+                }
+            });
+
+            // add layer to map & cache object
+            map.addLayer(layer);
+            lay[name] = layer;
         }
     });
 }
 
-function removeGeojsonPoints(name) {
+function removeLayer(name) {
     if (name in lay) {
         map.removeLayer(lay[name]);
     }
