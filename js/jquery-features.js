810e0ed7a6a172ac0dc6c8a90771c9ce6c67a36c
Piatinskii M <zenn1989@gmail.com>
2021-07-22 16:08:29 +0300

    update leaflet & add 2gis support instead of yandex

--- js/jquery-features.js
+++ js/jquery-features.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
     var alertAmShown = false;
     var alertFShown = false;
 
@@ -6,27 +6,27 @@ $(function(){
     $('.gis-group-layer').on('click', function(e) {
         var clickedTargetName = $(e.target).prop('tagName');
         var clickedTargetClass = $(e.target).prop('class');
-        if((clickedTargetName == "INPUT" || clickedTargetName === "LABEL" || clickedTargetName === "A") 
-            || clickedTargetClass.indexOf('gis-subgroup') !== -1) {
+        if ((clickedTargetName == "INPUT" || clickedTargetName === "LABEL" || clickedTargetName === "A") ||
+            clickedTargetClass.indexOf('gis-subgroup') !== -1) {
             return;
         }
-        
+
         var triggerDom = $(this).find('.gis-element-items');
         if (triggerDom.hasClass('d-none')) {
             $('.gis-element-items').addClass('d-none');
         }
         triggerDom.toggleClass('d-none');
     });
-    $('.gis-subgroup-lvl1-title').on('click', function(e){
-            var targetElement = $(this).parent().find('.gis-subroup-lvl1-content');
+    $('.gis-subgroup-lvl1-title').on('click', function(e) {
+        var targetElement = $(this).parent().find('.gis-subroup-lvl1-content');
 
-            if (targetElement.hasClass('d-none')) {
-                $('.gis-subroup-lvl1-content').addClass('d-none');
-            }
-            targetElement.toggleClass('d-none');
+        if (targetElement.hasClass('d-none')) {
+            $('.gis-subroup-lvl1-content').addClass('d-none');
+        }
+        targetElement.toggleClass('d-none');
     });
 
-    $('.gis-subgroup-lvl2-title').on('click', function(){
+    $('.gis-subgroup-lvl2-title').on('click', function() {
         var tElem = $(this).parent().find('.gis-subgroup-lvl2-content');
 
         if (tElem.hasClass('d-none')) {
@@ -37,16 +37,16 @@ $(function(){
     });
 
     // show warning
-    $('#show-amauter-warning').on('click', function(){
+    $('#show-amauter-warning').on('click', function() {
         if (!alertAmShown) {
             confirm("Внимание! Данные карты (с сопутствующей атрибутивной информацией) являются схематическим отображением Правил рыболовства и не обладают юридической силой. " +
-            "Полный текст Правил рыболовства для Азово-Черноморского рыбохозяйственного бассейна доступен по ссылке: " +
-            "http://ivo.garant.ru/#/document/70494670");
+                "Полный текст Правил рыболовства для Азово-Черноморского рыбохозяйственного бассейна доступен по ссылке: " +
+                "http://ivo.garant.ru/#/document/70494670");
             alertAmShown = true;
         }
     });
 
-    $('#show-fishing-warning').on('click', function(){
+    $('#show-fishing-warning').on('click', function() {
         if (!alertFShown) {
             confirm("Внимание! Данные карты (с сопутствующей атрибутивной информацией) являются схематическим отображением Правил рыболовства и не обладают юридической силой. " +
                 "Полный текст Правил рыболовства для Азово-Черноморского рыбохозяйственного бассейна доступен по ссылке: " +
@@ -56,7 +56,7 @@ $(function(){
     });
 
     // process geojson data display or hide based on checkbox status
-    $('input[type=checkbox]').on('change', function(){
+    $('input[type=checkbox]').on('change', function() {
         var checked = $(this).is(':checked');
 
         var layerName = $(this).attr('data-target');
@@ -65,14 +65,14 @@ $(function(){
 
         // prepare safe name for additional id tree element display
         var layerIdName = layerName.replace(":", "-_-");
-        
+
         if (!layerName || layerName.length < 3 || !layerType || layerType.length < 3) {
             return false;
         }
-        
+
         // process "check all" checkbox features
         if (layerType == "checkall") {
-            layerName.split(",").forEach(function(item){
+            layerName.split(",").forEach(function(item) {
                 $('#' + item).prop('checked', checked).trigger('change');
             });
         }
@@ -86,29 +86,29 @@ $(function(){
             displayLayerType(layerType, layerName, map, layerColor);
             enabledLayers[layerName] = true;
             // display tree button with "more" action
-            $(this).parent().find('label').after(" <a href='javascript:void(0)' id='modaltree-"+layerIdName+"' class='modal-tree-display'>🔎</a>");
+            $(this).parent().find('label').after(" <a href='javascript:void(0)' id='modaltree-" + layerIdName + "' class='modal-tree-display'>🔎</a>");
         } else {
             removeLayer(layerName);
             enabledLayers[layerName] = false;
-            $(this).parent().find("a#modaltree-"+layerIdName).remove();
+            $(this).parent().find("a#modaltree-" + layerIdName).remove();
         }
     });
     // process zoom buttons
-    $('#gis-zoom-azov').on('click', function(){
+    $('#gis-zoom-azov').on('click', function() {
         map.setView([46.1, 36.5], 8);
     });
-    $('#gis-zoom-blacksea').on('click', function(){
+    $('#gis-zoom-blacksea').on('click', function() {
         map.setView([43.8, 36.2], 8);
     });
-    $('#gis-zoom-rostov').on('click', function(){
+    $('#gis-zoom-rostov').on('click', function() {
         map.setView([47.2, 39.7], 9);
     });
-    $('#gis-zoom-krasnodar').on('click', function(){
+    $('#gis-zoom-krasnodar').on('click', function() {
         map.setView([45.0, 38.9], 9);
     });
-    
+
     // change tile layer
-    $('input[name=gis-tile]:radio').change(function(){
+    $('input[name=gis-tile]:radio').change(function() {
         map.removeLayer(bg);
         var mid = $(this).attr('id');
         if (mid === 'gis-tile-azniirkh') {
@@ -120,36 +120,33 @@ $(function(){
         }
         if (mid === 'gis-tile-yandex') {
             bg = L.tileLayer(
-              'https://sat{s}.maps.yandex.net/tiles?l=sat&v=3.378.0&z={z}&x={x}&y={y}&lang=ru_RU', {
-                subdomains: ['01', '02', '03', '04'],
-                attribution: '<a href="https://yandex.ru" target="_blank">Яндекс</a> | <a href="http://azniirkh.ru">АзНИИРХ</a>',
-                reuseTiles: true,
-                updateWhenIdle: false
-              }
+                'https://sat{s}.maps.yandex.net/tiles?l=sat&v=3.378.0&z={z}&x={x}&y={y}&lang=ru_RU', {
+                    subdomains: ['01', '02', '03', '04'],
+                    attribution: '<a href="https://yandex.ru" target="_blank">yandex.ru</a> | <a href="http://azniirkh.ru">АзНИИРХ</a>',
+                    reuseTiles: true,
+                    updateWhenIdle: false
+                }
             ).addTo(map);
             map.options.crs = L.CRS.EPSG3395;
         }
         if (mid === 'gis-tile-yandex-scheme') {
-            bg = L.tileLayer(
-              'https://vec{s}.maps.yandex.net/tiles?l=map&v=18.06.17-0&z={z}&x={x}&y={y}&lang=ru_RU', {
-                subdomains: ['01', '02', '03', '04'],
-                attribution: '<a href="https://yandex.ru" target="_blank">Яндекс</a> | <a href="http://azniirkh.ru">АзНИИРХ</a>',
+            bg = L.tileLayer('http://tile2.maps.2gis.com/tiles?x={x}&y={y}&z={z}', {
+                attribution: '<a href="https://2gis.ru" target="_blank">2gis.ru</a> | <a href="http://azniirkh.ru">АзНИИРХ</a>',
                 reuseTiles: true,
                 updateWhenIdle: false
-              }
-            ).addTo(map);
+            }).addTo(map);
             map.options.crs = L.CRS.EPSG3395;
         }
-        
+
         var zoomIdx = map.getZoom();
-        map.setZoom(zoomIdx+1);
-        setTimeout(function(){
+        map.setZoom(zoomIdx + 1);
+        setTimeout(function() {
             map.setZoom(zoomIdx);
         }, 800);
     });
 
     // display modal window on icon click
-    $(document).on('click', '.modal-tree-display', function(){
+    $(document).on('click', '.modal-tree-display', function() {
         var treeId = $(this).attr('id');
         var layerName = treeId.replace("-_-", ":").replace("modaltree-", "");
         var modal = $('#modalTreeDisplay');
@@ -165,7 +162,7 @@ $(function(){
         var html = '<div class="table-responsive"><table class="table" id="modal-tree-table">';
         html += '<thead><tr><td>Название</td><td>Действия</td></tr></thead>';
         html += '<tbody>';
-        
+
         var lyr = lay[layerName];
         for (var k in lyr._layers) {
             var feature = lyr._layers[k];
@@ -178,9 +175,9 @@ $(function(){
             }
 
             var featureId = feature.feature.id;
-            var action = '<a href="javascript:void(0)" class="moveToLeafletLayer" data-group="'+layerName+'" data-id="layer-' + feature._leaflet_id + '">Просмотр</a>';
+            var action = '<a href="javascript:void(0)" class="moveToLeafletLayer" data-group="' + layerName + '" data-id="layer-' + feature._leaflet_id + '">Просмотр</a>';
 
-            html += '<tr id="tree-row-'+feature._leaflet_id+'"><td class="modal-search-index">' + text + '</td><td>' + action + '</td></tr>';
+            html += '<tr id="tree-row-' + feature._leaflet_id + '"><td class="modal-search-index">' + text + '</td><td>' + action + '</td></tr>';
             //console.log(feature);
         }
 
@@ -192,7 +189,7 @@ $(function(){
     });
 
     // process click event on legend features - zoom to layer and show popup
-    $(document).on('click', '.moveToLeafletLayer', function(){
+    $(document).on('click', '.moveToLeafletLayer', function() {
         var oid = $(this).attr('data-id').replace('layer-', '');
         var layerName = $(this).attr('data-group');
 
@@ -211,7 +208,7 @@ $(function(){
     });
 
     // process modal search
-    $('#modal-search-btn').on('click', function(){
+    $('#modal-search-btn').on('click', function() {
         var query = $('#modal-search-field').val();
         if (query == '' || query.length < 3) {
             alert("Запрос слишком короткий!");
@@ -221,7 +218,7 @@ $(function(){
 
         var tableSelector = $('#modal-tree-table');
         var foundIdx = 0;
-        tableSelector.find('td.modal-search-index').each(function(){
+        tableSelector.find('td.modal-search-index').each(function() {
             var row = this.innerText.toLowerCase();
             // check if row contains search query
             if (row.indexOf(query) !== -1) {
@@ -233,7 +230,7 @@ $(function(){
                 $(this).parent().removeClass('alert-success');
             }
         });
-        
+
         $('#modal-found-text').removeClass("d-none");
         $('#modal-found-count').text(foundIdx);
     });
